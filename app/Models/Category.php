97e00fbc1fa729c8a1fb813efaadd572cef01962
fxf28 <faizfjr666@gmail.com>
2025-09-23 12:58:36 +0700

    add model event

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Log;
 
 class Category extends Model
 {
-    use HasFactory;
-
     protected $fillable = ['name'];
+
+    protected static function booted()
+    {
+        // Event yang dijalankan sebelum kategori dihapus
+        static::deleting(function ($category) {
+            // Hapus semua gambar Cloudinary dari post yang terkait
+            foreach ($category->posts as $post) {
+                if ($post->image_public_id) {
+                    try {
+                        \CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::uploadApi()->destroy($post->image_public_id);
+                        Log::info("Cloudinary image deleted from category deletion: " . $post->image_public_id);
+                    } catch (\Exception $e) {
+                        Log::error("Cloudinary delete error (category): " . $e->getMessage());
+                    }
+                }
+            }
+        });
+    }
 
     public function posts(): HasMany
     {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class Post extends Model
 {
@@ -14,6 +16,22 @@ class Post extends Model
         'text',
         'category_id'
     ];
+
+    protected static function booted()
+    {
+        // Event yang dijalankan sebelum post dihapus
+        static::deleting(function ($post) {
+            // Hapus gambar dari Cloudinary
+            if ($post->image_public_id) {
+                try {
+                    Cloudinary::uploadApi()->destroy($post->image_public_id);
+                    Log::info("Cloudinary image deleted: " . $post->image_public_id);
+                } catch (\Exception $e) {
+                    Log::error("Cloudinary delete error: " . $e->getMessage());
+                }
+            }
+        });
+    }
 
     public function category(): BelongsTo
     {
